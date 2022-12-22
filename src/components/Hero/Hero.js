4c641183c0faa8da3c6f799ae280ae4b4423b5bb
Isabelle Huang <102778105+isabellehuang@users.noreply.github.com>
2022-12-22 01:48:17 -0500

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => (
       <LeftSection>
         <TextAnimation>Hi, I'm Isabelle Huang.</TextAnimation>
         <SectionText>
-          I am first-year Computer Engineering student at the University of Waterloo. Recently, I've enjoyed dance, snowboarding, and familiarizing myself with Leetcode.
+          I am first-year Computer Engineering student at the University of Waterloo. Recently, I've enjoyed snowboarding and familiarizing myself with Leetcode.
           <br /> <br /> Last Updated: December 21, 2022
         </SectionText>
         <SecondaryBtn onClick = {() => window.location = 'https://google.com'}> Say Hello!</SecondaryBtn>
